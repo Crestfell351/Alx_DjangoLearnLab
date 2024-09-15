6928697e7a28a157d6ec4e96e00a078a3b25bf40
Aphiwe Makhathini <vmelamore@gmail.com>
2024-09-15 23:07:29 +0200

    t

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,6 +13,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author'] 
     ordering_fields = ['title', 'publication_year'] 

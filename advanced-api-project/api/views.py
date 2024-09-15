19dfca036a3e525af4fca1c31d6105d0554c4391
Aphiwe Makhathini <vmelamore@gmail.com>
2024-09-15 22:53:35 +0200

    Sonto

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,11 +2,18 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 # List all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author'] 
+    ordering_fields = ['title', 'publication_year'] 
 
 # Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
